1f20bc6205e954a8792a8c594adcddd653a45678
Amarjeet Singh <200390amarjeet@gmail.com>
2024-01-13 21:41:52 +0530

    Windows code pushed to linux

---
+++ credit_app/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+
+class Customer(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    phone_number = models.CharField(max_length=15)
+    monthly_salary = models.DecimalField(max_digits=10, decimal_places=2)
+    approved_limit = models.DecimalField(max_digits=10, decimal_places=2)
+    current_debt = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+class Loan(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    loan_amount = models.DecimalField(max_digits=10, decimal_places=2)
+    tenure = models.IntegerField()
+    interest_rate = models.DecimalField(max_digits=5, decimal_places=2)
+    monthly_repayment = models.DecimalField(max_digits=10, decimal_places=2)
+    emis_paid_on_time = models.IntegerField()
+    start_date = models.DateField()
+    end_date = models.DateField()
+
+    def __str__(self):
+        return f"Loan for {self.customer}: {self.loan_amount}"
+    
